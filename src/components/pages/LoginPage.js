d20bab65f8c7a479c56ea9b2bc165b091a7072bb
Aaron Fischer <aaron.fischer@caxiam.com>
2018-01-19 00:51:56 -0500

    Login form with clientside validation

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import LoginForm from '../forms/LoginForm';
 
-const LoginPage = () => (
-  <div>
-    <h1>Login Page</h1>
-  </div>
-);
+class LoginPage extends Component {
+  submit = data => {
+    console.log('data', data);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Login Page</h1>
+
+        <LoginForm submit={this.submit} />
+      </div>
+    );
+  }
+};
 
 export default LoginPage;
