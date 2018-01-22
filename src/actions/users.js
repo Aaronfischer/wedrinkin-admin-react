982a72dcee6516aabfdee4261c8ca7daf7b51023
Aaron Fischer <aaron.fischer@caxiam.com>
2018-01-21 21:42:26 -0500

    Store user after signup

--- src/actions/users.js
+++ src/actions/users.js
@@ -3,5 +3,6 @@ import { userLoggedIn } from './auth';
 
 export const signup = data => dispatch =>
   api.user.signup(data).then(user => {
+    localStorage.wedrinkinJWT = user.token;
     dispatch(userLoggedIn(user));
   });
