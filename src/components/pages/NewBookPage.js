19db7d452ee2ef1c05c522dcde829983cb7e39cb
Aaron Fischer <aaron.fischer@caxiam.com>
2018-01-27 13:46:00 -0500

    Setup newbook form and search book form

--- src/components/pages/NewBookPage.js
+++ src/components/pages/NewBookPage.js
@@ -2,17 +2,24 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Segment } from 'semantic-ui-react';
 import SearchBookForm from '../forms/SearchBookForm';
+import BookForm from '../forms/BookForm';
 
 export class NewBookPage extends React.Component {
   state = {
     book: null
   };
 
+  onBookSelect = book => this.setState({ book });
+
+  addBook = () => console.log('hi');
+
   render() {
     return (
       <Segment>
         <h1>Add new book to your collection</h1>
-        <SearchBookForm />
+        <SearchBookForm onBookSelect={this.onBookSelect} />
+
+        {this.state.book && <BookForm submit={this.addBook} book={this.state.book} />}
       </Segment>
     );
   }
